ac6b575596c266b718722b42b6858c0fc74a4788
Salman Khan <114092557+povsalman@users.noreply.github.com>
2025-07-27 13:00:56 +0500

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,11 +1,10 @@
-
 from model import PointCloudNet
 from utils import predict
 import torch
 
 
 
-model_save_name = "pc1024_three.pth"
+model_save_name = "/content/drive/MyDrive/pc1024_three.pth"
 
 model = PointCloudNet(num_views=1, point_cloud_size=1024, num_heads=4, dim_feedforward=2048)
 model.load_state_dict(torch.load(model_save_name)["model"])
